e5316d0afa8030addcf244161261454e695a355f
Justin Lilly <justinlilly@gmail.com>
2008-09-18 11:15:57 -0400

    Added view for filtering by agency

--- blotter/views.py
+++ blotter/views.py
@@ -3,10 +3,9 @@ from crime_map.blotter.models import Crime
 
 
 def by_type(request, crime_type):
-    #import ipdb; ipdb.set_trace()
-    print repr(crime_type)
     crimes = Crime.objects.filter(crime_type__slug=crime_type)
     return object_list(request, queryset=crimes)
 
-def by_agency():
-    pass
+def by_agency(request, agency):
+    crimes = Crime.objects.filter(agency__slug=agency)
+    return object_list(request, queryset=crimes)
