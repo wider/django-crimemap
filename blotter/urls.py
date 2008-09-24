bff99d60d7ab271a70cb5010de8911479998c9e6
Justin Lilly <justinlilly@gmail.com>
2008-09-24 11:57:12 -0400

    URL entry for reporter's output

--- blotter/urls.py
+++ blotter/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', dict({'queryset':Crime.objects.approved(), 'date_field':'date', 'make_object_list':True}), name="crime-list-by-year"),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', dict({'queryset':Crime.objects.approved(), 'date_field':'date'}), name="crime-list-by-month"),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/$', 'django.views.generic.date_based.archive_day', dict({'queryset':Crime.objects.approved(), 'date_field':'date'}), name="crime-list-by-day"),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/output/$', 'django.views.generic.date_based.archive_day', dict({'queryset':Crime.objects.approved(), 'date_field':'date', 'template_name':'blotter/output.html'}), name="crime-list-by-day-output"),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<object_id>\d+)/$', 'django.views.generic.date_based.object_detail', dict({'queryset':Crime.objects.approved(), 'date_field':'date'}), name="crime-instance"),
 
     # Crimes by Type
